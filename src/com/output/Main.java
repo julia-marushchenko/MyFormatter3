c9361e1f301bf9b4952904d3ebc31e4cfb3f6ecd
Julia Marushchenko <marushchenko.yulia@gmail.com>
2025-04-13 04:56:40 +0200

    Add files via upload
    Commiting Main class

---
+++ src/com/output/Main.java
@@ -0,0 +1,13 @@
+package com.output;// Java program to represent character Unicode with word Hello
+// Java program to represent character Unicode with word Hello
+
+// Class Main
+public class Main {
+    public static void main(String[] args) {
+
+        // Representing character unicodes
+        String str = String.format("%c%c%c%c%c", 72, 101, 108,108,111);
+        System.out.printf(str);
+
+    }
+}
